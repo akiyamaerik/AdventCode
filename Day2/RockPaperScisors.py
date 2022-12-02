36c3a0e5c37dbf95a2a6e19c6427a77d66a98a12
Erik Akiyama <erik.akiyama@volvo.com>
2022-12-02 08:56:55 +0000

    Round win,loss and tie are working

--- Day2/RockPaperScisors.py
+++ Day2/RockPaperScisors.py
@@ -2,25 +2,28 @@ f = open("input.txt","rt") #open file in read text mode
 win  = 0
 loss = 0
 tie  = 0
+pick = 0
 for line in f:
+   line=line.rstrip()
    if line == 'A X':
-      tie+=1
+      tie = tie+1
    elif line == 'A Y':
-      loss+=1
+      loss = loss+1
    elif line == 'A Z':
-      win+=1
+      win= win+1
    elif line == 'B X':
-      win+=1
+      win= win+1
    elif line == 'B Y':
-      tie+=1
+      tie = tie+1
    elif line == 'B Z':
-      loss+=1
+      loss = loss+1
    elif line == 'C X':
-      loss+=1
+      loss = loss+1
    elif line == 'C Y':
-      win+=1
+      win= win+1
    elif line == 'C Z':
-      tie+=1
+      tie = tie+1
+
 print "Win:", win
 print "Tie:", tie
 print "Loss:", loss
