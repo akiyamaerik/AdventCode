3143745a08ed7c53fb019f9d01cd476664917d20
Erik Akiyama <erik.akiyama@volvo.com>
2022-12-02 12:25:37 +0000

    Added comments for Day2Part2

--- Day2/RockPaperScisors.py
+++ Day2/RockPaperScisors.py
@@ -5,31 +5,31 @@ tie  = 0
 score = 0
 for line in f:
    line=line.rstrip()
-   if line == 'A X': #rock rock
+   if line == 'A X': #rock rock Need to Lose
       tie+=1
       score+=4
-   elif line == 'A Y': #rock paper
+   elif line == 'A Y': #rock paper Need to Draw
       win+=1
       score+=8
-   elif line == 'A Z': #rock scissors
+   elif line == 'A Z': #rock scissors Need to Win
       loss+=1
       score+=3
-   elif line == 'B X':#paper rock
+   elif line == 'B X':#paper rock Need to Lose
       loss+=1
       score+=1
-   elif line == 'B Y': #paper paper
+   elif line == 'B Y': #paper paper Need to Draw
       tie+=1
       score+=5
-   elif line == 'B Z': #paper scissors
+   elif line == 'B Z': #paper scissors Need to Win
       loss+=1
       score+=9
-   elif line == 'C X': #scissors rock
+   elif line == 'C X': #scissors rock Need to Lose
       win+=1
       score+=7
-   elif line == 'C Y': #scissors paper
+   elif line == 'C Y': #scissors paper Need to Draw
       loss+=1
       score+=2
-   elif line == 'C Z': #scissors scissors
+   elif line == 'C Z': #scissors scissors Need to Win
       tie+=1
       score+=6
 
