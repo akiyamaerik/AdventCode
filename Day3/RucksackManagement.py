49d8ad1513ee792f30b4811ca8702e7bf8ca358d
Erik Akiyama <akiyama.erik@gmail.com>
2022-12-03 15:00:24 +0100

    Working code returning number priority

--- Day3/RucksackManagement.py
+++ Day3/RucksackManagement.py
@@ -1,5 +1,21 @@
 f = open("input.txt","rt") #open file in read text mode'
+priority = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 line=0
+
+# retrieve position of a letter in the alphabet set
+def position(letter):
+  pos = priority.index(letter) + 1
+  return pos
+
+# function to recursively find the column number  
+def column_number(label, n):
+  if n==1:
+    return position(label)
+
+  else:
+    return ((26**(n-1)) * position(label[0])) + column_number(label[1:], n-1)
+
+#main code
 for line in f:
    line=line.rstrip()
    length=len(line) #Checking string lenght
@@ -14,7 +30,12 @@ for line in f:
 
    print("The common letters are:")
    for i in a:
-      print(i)  
+      print(i)
+      n = len(i)
+
+      col_n = column_number(i, n)
+      print(col_n)
+
 #   print("Part1",line[part1],"Part2",line[part2])
 #   print ("Line lenght:",length)
 f.close()
