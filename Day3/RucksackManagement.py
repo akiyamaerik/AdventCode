d6884b71d40aadd5dd3c74f88cb41ccc7801c19e
Erik Akiyama <akiyama.erik@gmail.com>
2022-12-03 14:22:05 +0100

    Splitting string in half

--- Day3/RucksackManagement.py
+++ Day3/RucksackManagement.py
@@ -3,5 +3,8 @@ line=0
 for line in f:
    line=line.rstrip()
    length=len(line)
+   part1 = slice(0,len(line)//2)
+   part2 = slice(len(line)//2,len(line))
+   print("Part1",line[part1],"Part2",line[part2])
    print ("Line lenght:",length)
 f.close()
